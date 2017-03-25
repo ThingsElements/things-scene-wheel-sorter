00a09a7e2907caeced5d42f59cc1aea5767dcfc6
khw <kang-h@github.com>
2017-03-25 14:08:37 +0900

    wheel-sorter
    제작중

--- src/wheel-sorter.js
+++ src/wheel-sorter.js
@@ -36,8 +36,30 @@ export default class WheelSorter extends RectPath(Component) {
   onchange(after, before) {
   }
 
-  _draw(context) {
-    super._draw(context)
+  _draw(ctx) {
+    //super._draw(ctx)
+
+    var {
+      width, height, left, top,
+      fillStyle = '#fff',
+      strokeStyle = '#666',
+      lineWidth = 1
+    } = this.model;
+
+    ctx.beginPath();
+    ctx.rect(left, top, width, height);
+    ctx.stroke();
+
+    for(let j = 1; j < 10; j += 2){
+      for(let i = 1; i < 10; i += 2){
+        ctx.beginPath();
+        ctx.fillStyle = fillStyle
+        ctx.strokeStyle = strokeStyle
+        ctx.lineWidth = lineWidth
+        ctx.ellipse(left + (width / 10) * i, top + (width / 10) * j, width / 12, width / 12, 0, 0, 2 * Math.PI, 0);
+        ctx.stroke();
+      }
+    }
   }
 }
 
