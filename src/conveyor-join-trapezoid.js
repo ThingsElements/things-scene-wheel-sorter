11a3b2e531bc5619d6694a0bbbf0c4dad3210550
khw <kang-h@github.com>
2017-03-27 17:10:59 +0900

    폴리곤 형식의 join 추가
    .

---
+++ src/conveyor-join-trapezoid.js
@@ -0,0 +1,31 @@
+import MixinRoller from './mixin-roller'
+
+var {
+  Component,
+  Polygon
+} = scene;
+
+const NATURE = {
+  mutable: false,
+  resizable: true,
+  rotatable: true,
+  properties : [{
+    type: 'number',
+    label: 'value',
+    name: 'value',
+    property: 'value'
+  }]
+}
+
+export default class ConveyorJoinTrapezoid extends MixinRoller(Polygon) {
+
+  get nature() {
+    return NATURE
+  }
+
+  is3dish() {
+    return true
+  }
+}
+
+Component.register('conveyor-join-trapezoid', ConveyorJoinTrapezoid);
